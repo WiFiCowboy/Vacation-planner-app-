7f1f605fc1dc920be19d10d4324ae9a5f1ca561a
Humberto Hernandez <hamburger45@gmail.com>
2019-11-16 17:16:20 -0800

    added script for fetch

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let states = '';
+const apiKey = 'l9Jpa2gHBgJQ7jQ7pJEdjagQaoDAuFkoGbbJSv3x@';
+const searchURL = 'developer.nps.gov/api/v1/parks';
+const queryParam = '?stateCode=';
+
+// Example url for CA https://developer.nps.gov/api/v1/parks?stateCode=ca
+
 
 // The user must be able to search for parks in one or more states.
 function getStates(){
@@ -6,12 +12,51 @@ function getStates(){
         event.preventDefault();
         states = $('#states').val().toLowerCase();
         console.log(states);
+        grabMaxResults();
     });
 };
 
 
 // The user must be able to set the max number of results, with a default of 10.
+function grabMaxResults() {
+    let maxResults = '';
+    maxResults = $('#returnResults').val(); 
+    console.log(maxResults);
+    
+}
+
 // The search must trigger a call to NPS's API.
+function getUserRepo(){
+    fetch('https://' + apiKey + searchURL + queryParam + states)
+    .then(response => {
+        if(response.ok){
+            return response.json();
+        }else{
+            // skips next .then and goes to .catch
+            throw new Error();
+        }
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => errorHandle());
+};
+
+// function displayResults(responseJson){
+//     resetResults();
+//     for(let i = 0; i < responseJson.length; i++){
+//         $('.displayParkInfo').append(
+//             `<h2>Repo Name: ${responseJson[i].name}</h2>
+//             <a href="${responseJson[i].html_url}">${responseJson[i].html_url}</a>`
+//           )
+//     }
+//     $('.results').removeClass('hidden');
+// };
+
+function errorHandle() {
+    resetResults();
+    $('.results').append(`<h3>States not found!</h3>`);
+    $('.results').removeClass('hidden');
+};
+
 // The parks in the given state must be displayed on the page. Include at least:
 // Full name
 // Description
@@ -23,6 +68,7 @@ function getStates(){
 function masterControl(){
     console.log('Script connected');
     getStates();
+
 };
 
 $(masterControl);
